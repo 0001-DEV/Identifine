447e8608270c69e1c30f5f9830e8bcc9f344498f
WEBLOVE <loveolaoye2@gmail.com>
2026-08-19 12:24:27 +0100

    Shift left contact details further left with negative margin

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -26,7 +26,7 @@ export default function ContactPage() {
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 items-start">
           
           {/* Left Details */}
-          <div className="lg:col-span-4 space-y-10">
+          <div className="lg:col-span-4 space-y-10 lg:-ml-8 xl:-ml-12">
             
             {/* Contact us */}
             <div className="space-y-3">
