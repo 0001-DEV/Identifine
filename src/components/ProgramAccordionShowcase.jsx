9faa02dcf835c77d9088541b990301236baa1f00
WEBLOVE <loveolaoye2@gmail.com>
2026-08-20 12:58:24 +0100

    Reduce image height further in Our identity experience program section to 500px

--- src/components/ProgramAccordionShowcase.jsx
+++ src/components/ProgramAccordionShowcase.jsx
@@ -121,8 +121,8 @@ export default function ProgramAccordionShowcase() {
         })}
       </div>
 
-      {/* DYNAMIC IMAGE (RIGHT COLUMN: 6 Cols on Desktop - Reduced height by 12px) */}
-      <div className="lg:col-span-6 order-2 relative w-full h-[408px] sm:h-[508px] lg:h-[608px] rounded-3xl overflow-hidden bg-[#111111] border border-[#DCDAD4] shadow-2xl min-h-[408px] sm:min-h-[508px] lg:min-h-[608px]">
+      {/* DYNAMIC IMAGE (RIGHT COLUMN: 6 Cols on Desktop - Sleek reduced height) */}
+      <div className="lg:col-span-6 order-2 relative w-full h-[360px] sm:h-[440px] lg:h-[500px] rounded-3xl overflow-hidden bg-[#111111] border border-[#DCDAD4] shadow-2xl min-h-[360px] sm:min-h-[440px] lg:min-h-[500px]">
         {programs.map((prog) => {
           const isActive = activeId === prog.id;
           return (
